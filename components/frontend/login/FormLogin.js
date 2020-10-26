7cea650ea30484b6f785aa27c935e6ee6463681b
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-10-26 22:49:31 +0700

    update

---
+++ components/frontend/login/FormLogin.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import InputLabel from '../../widget/InputLabel'
+  
+const FormLogin = (props) => {
+  const {error} = props;
+
+ 
+
+  return (
+    <>
+      <InputLabel inputProps={{ 
+        className:'form-control', type : 'email',name : 'username', id : "email",
+        pattern : "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$", title :"Invalid email", required : true
+      }} 
+      labelName="Email" iconProps={{className : 'fa icon icon-email'}}  />
+      <InputLabel inputProps={{ 
+        className:'form-control', type : 'password',name : 'password', id : "password",
+        pattern : ".{6,}", title :"six or more characters", required : true
+      }} 
+      labelName="Password" iconProps={{className : 'fa icon icon-lock-1'}}  />
+      {
+        error && ( 
+          <div className="text-danger"> Email or password is incorrect </div>
+        )
+      }
+
+      <div className="clearfix add_bottom_30">
+        <div className="float-right mt-1"><a id="forgot" href="javascript:void(0);">Forgot Password?</a></div>
+      </div>
+
+      <div>
+        <button type="submit"  className="btn_1 rounded full-width">Login</button>
+      </div>
+
+      <div className="divider"><span>Or</span></div>
+      
+      <div className="access_social">
+        <a href="#0" className="social_bt facebook">Login with Facebook</a>
+      </div>
+    </>
+  )
+}
+export default FormLogin
--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,8 @@ import Layout from '../components/frontend/layout/Layout';
 import api from '../utils/api'
 import AuthService from '../utils/AuthService'
 import UserContext from '../contexts/UserContext';
+import InputLabel from '../components/widget/InputLabel'
+import FormLogin from '../components/frontend/login/FormLogin'
 
 const Login = ({ t }) => {
   const [loading, setLodding] = useState(false);
@@ -38,37 +40,7 @@ const Login = ({ t }) => {
       <div className="container" id="login">
         <aside className="main-content">
           <form id="login-form" onSubmit={login}>
-            <div className="form-group">
-              <label>Email</label>
-              <input type="email" className="form-control" name="username" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Invalid email" required />
-              <i className="icon_mail_alt"></i>
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input type="password" className="form-control" name="password" id="password" pattern=".{6,}" title="six or more characters" required />
-              <i className="icon_lock_alt"></i>
-            </div>
-
-            {
-              error && ( 
-                <div className="text-danger"> Email or password is incorrect </div>
-              )
-            }
-
-            <div className="clearfix add_bottom_30">
-              <div className="checkboxes float-left">
-                <label className="container_check">Remember me
-                  <input type="checkbox" />
-                  <span className="checkmark"></span>
-                </label>
-              </div>
-              <div className="float-right mt-1"><a id="forgot" href="javascript:void(0);">Forgot Password?</a></div>
-            </div>
-            <button type="submit"  className="btn_1 rounded full-width">Login</button>
-            <div className="divider"><span>Or</span></div>
-            <div className="access_social">
-              <a href="#0" className="social_bt facebook">Login with Facebook</a>
-            </div>
+            <FormLogin error={error} />
           </form>
         </aside>
       </div>
