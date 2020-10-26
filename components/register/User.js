95d5c709109df0049bfdbee791876fc51d628cf1
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-10-26 21:16:20 +0700

    update

--- components/register/User.js
+++ components/register/User.js
@@ -10,7 +10,10 @@ const User = (props) => {
     setShow(3);
   }
 
-  const optionDistrict = [{val : 'คลองต้นไทร', name : 'คลองต้นไทร'}];
+  const optionPosition = [
+    {val : '1', name : 'Sale'},{val : '2', name : 'Sales Agent'},{val : '3', name : 'Sales & Operation'},
+    {val : '4', name : 'Tour Operation'},{val : '5', name : 'Operations Executive'},{val : '6', name : 'Operation Manager '}
+  ];
 
   return (
     <>
@@ -29,7 +32,7 @@ const User = (props) => {
         <div className="row justify-content-center">
           <div className="col-lg-6 col-12">
             <SelectLabel inputProps={{ className:'form-control select', name : 'position'}} 
-            labelName="Position : ตำแหน่ง" iconProps={{className : 'icon_lock_alt'}}  />
+            labelName="Position : ตำแหน่ง" iconProps={{className : 'icon_lock_alt'}} options={optionPosition} />
           </div>
           <div className="col-lg-6 col-12">
             <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'phone'}} 
