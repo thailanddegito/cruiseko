2b53a00dde45ca3480a92e4b04cabf33483056c6
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-10-26 20:38:18 +0700

    update

--- components/register/Success.js
+++ components/register/Success.js
@@ -12,43 +12,38 @@ const Success = (props) => {
   return (
     <>
       <div className={`${show ? 'd-block' : 'd-none'}`}>
-        <div className="container">
+        <div className="row justify-content-start">
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'id', readOnly : true}} 
+              labelName="Account ID : " iconProps={{className : 'icon_lock_alt'}}  />
+          </div>
+        </div>
 
-          <div className="row justify-content-start">
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'id', readOnly : true}} 
-                labelName="Account ID : " iconProps={{className : 'icon_lock_alt'}}  />
+        <div className="row justify-content-start">
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'username'}} 
+            labelName="Username : " iconProps={{className : 'icon_lock_alt'}}  />
+          </div>
+        </div>
+        <div className="row justify-content-start">
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'password'}} 
+            labelName="Password : " iconProps={{className : 'icon_lock_alt'}}  />
+          </div>
+        </div>
+        <div className="row justify-content-start">
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text'}} 
+            labelName="Re - Password : " iconProps={{className : 'icon_lock_alt'}}  />
+          </div>
+        </div>
+        
+        <div className="row justify-content-start">
+          <div className="col-12">
+            <div className="form-group">
+              <button type="button" className="btn btn-primary" onClick={() => saveStep3()}>บันทึก</button>
             </div>
           </div>
-
-          <div className="row justify-content-start">
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'username'}} 
-              labelName="Username : " iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
-          </div>
-          <div className="row justify-content-start">
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'password'}} 
-              labelName="Password : " iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
-          </div>
-          <div className="row justify-content-start">
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text'}} 
-              labelName="Re - Password : " iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
-          </div>
-          
-          <div className="row justify-content-start">
-            <div className="col-12">
-              <div className="form-group">
-               <button type="button" className="btn btn-primary" onClick={() => saveStep3()}>บันทึก</button>
-              </div>
-            </div>
-          </div>
-          
-
         </div>
       </div>
     </>
--- components/register/User.js
+++ components/register/User.js
@@ -12,51 +12,47 @@ const User = (props) => {
   return (
     <>
       <div className={`${show ? 'd-block' : 'd-none'}`}>
-        <div className="container">
-
-          <div className="row justify-content-center">
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'firstname'}} 
-              labelName="Name : ชื่อ" iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'lastname'}} 
-              labelName="Last Name : นามสกุล" iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'firstname'}} 
+            labelName="Name : ชื่อ" iconProps={{className : 'icon_lock_alt'}}  />
           </div>
-
-          <div className="row justify-content-center">
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'position'}} 
-              labelName="Position : ตำแหน่ง" iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'phone'}} 
-              labelName="Tel : เบอร์โทรศัพท์" iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'lastname'}} 
+            labelName="Last Name : นามสกุล" iconProps={{className : 'icon_lock_alt'}}  />
           </div>
-
-          <div className="row justify-content-center">
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'email'}} 
-              labelName="Email : อีเมล์" iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
-            <div className="col-lg-6 col-12">
-              <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'line_id'}} 
-              labelName="Line ID (ไม่จำเป็นต้องระบุ)" iconProps={{className : 'icon_lock_alt'}}  />
-            </div>
-          </div>
- 
-          <div className="row justify-content-start">
-            <div className="col-12">
-              <div className="form-group">
-               <button type="button" className="btn btn-primary" onClick={() => saveStep2()}>ขั้นตอนถัดไป</button>
-              </div>
-            </div>
-          </div>
-          
-
         </div>
+
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'position'}} 
+            labelName="Position : ตำแหน่ง" iconProps={{className : 'icon_lock_alt'}}  />
+          </div>
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'phone'}} 
+            labelName="Tel : เบอร์โทรศัพท์" iconProps={{className : 'icon_lock_alt'}}  />
+          </div>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'email'}} 
+            labelName="Email : อีเมล์" iconProps={{className : 'icon_lock_alt'}}  />
+          </div>
+          <div className="col-lg-6 col-12">
+            <InputLabel inputProps={{ className:'form-control', type : 'text',name : 'line_id'}} 
+            labelName="Line ID (ไม่จำเป็นต้องระบุ)" iconProps={{className : 'icon_lock_alt'}}  />
+          </div>
+        </div>
+
+        <div className="row justify-content-start">
+          <div className="col-12">
+            <div className="form-group">
+              <button type="button" className="btn btn-primary" onClick={() => saveStep2()}>ขั้นตอนถัดไป</button>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </>
   )
