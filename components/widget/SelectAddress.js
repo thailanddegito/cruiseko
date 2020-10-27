164dec61c69166b3e5191bcf368b3271aa564b06
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-10-27 15:27:41 +0700

    Update SelectAddress.js

--- components/widget/SelectAddress.js
+++ components/widget/SelectAddress.js
@@ -1,34 +1,115 @@
 
-import React from 'react';
-import SelectLabel from './SelectLabel';
-import InputLabel from './InputLabel';
+import React, { useEffect, useState } from 'react';
+import myData from '../../public/json/raw_database.json';
 
 const SelectAddress = (props) => {
   const {name} = props;
 
-  const optionProvince = [{val : '10', name : 'กรุงเทพมหานคร'}];
-  const optionAmphoe = [{val : 'คลองสาน', name : 'คลองสาน'}];
-  const optionDistrict = [{val : 'คลองต้นไทร', name : 'คลองต้นไทร'}];
-  const optionPost = [{val : '10600', name : '10600'}];
+  const [province, setProvince] = useState();
+  const [amphoe, setAmphoe] = useState();
+  const [district, setDistrict] = useState();
+  const [zipcode, setZipcode] = useState();
+
+  useEffect(() => {
+    getOptionAddress(myData)
+  },[]);
+
+  const getOptionAddress = (obj , index = 'กรุงเทพมหานคร')=>{
+    var prov = groupBy(obj,'province');
+    var amp = groupBy(prov[index],'amphoe')
+    var district = groupBy(amp[Object.keys(amp)[0]],'district')
+    setProvince(prov)
+    setAmphoe(amp)
+    setDistrict(district)
+    setZipcode(groupBy(district[Object.keys(district)[0]],'zipcode'))
+  }
+  // console.log(province);
+  const onChangeProv = (e) => {
+    getOptionAddress(myData,e.target.options[e.target.selectedIndex].text)
+  }
+
+  const getOptionAmphoe = (index = 0)=>{
+    var district = groupBy(amphoe[index],'district')
+    setDistrict(district)
+    setZipcode(groupBy(district[Object.keys(district)[0]],'zipcode'))
+    // console.log(groupBy(amphoe[index],'zipcode'));
+  }
+
+  const onChangeAmphoe = (e) => {
+    getOptionAmphoe(e.target.options[e.target.selectedIndex].text)
+  }
+
+  const getOptionDistrict = (index = 0)=>{
+    setZipcode(groupBy(district[index],'zipcode'))
+  }
+
+  const onChangeDistrict = (e) => {
+    getOptionDistrict(e.target.options[e.target.selectedIndex].text)
+  }
+
+  const groupBy = (array, key) => {
+    // Return the end result
+    return array.reduce((result, currentValue) => {
+      // If an array already present for key, push it to the array. Else create an array and push the object
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+      return result;
+    }, {}); // empty object is the initial value for result object
+  };
+
 
   return (
     <>
      <div className="row justify-content-center">
         <div className="col-lg-3 col-12">
-          <SelectLabel inputProps={{ className:'form-control select', name : 'province', required : true}} 
-          labelName="จังหวัด" iconProps={{className : 'fa icon icon-home'}} options={optionProvince}  />
+          <div className="form-group">
+            <label>จังหวัด</label>}
+            <select className="form-control" name={`province_code`} onChange={onChangeProv}>
+              {
+                province ? Object.keys(province).map((prov,index)=>(
+                  <option value={province[prov][0].province_code} key={prov}>{prov}</option>
+                )) : ''
+              }
+            </select>
+          </div>
         </div>
         <div className="col-lg-3 col-12">
-          <SelectLabel inputProps={{ className:'form-control select', name : 'amphoe', required : true}} 
-          labelName="อำเภอ" iconProps={{className : 'fa icon icon-home'}} options={optionAmphoe}  />
+          <div className="form-group">
+            <label>อำเภอ</label>}
+            <select className="form-control" name={`ampher`} onChange={onChangeAmphoe}>
+              {
+                amphoe ? Object.keys(amphoe).map((amp,index)=>(
+                  <option value={amp} key={amp}>{amp}</option>
+                )) : ''
+              }
+            </select>
+          </div>
         </div>
         <div className="col-lg-3 col-12">
-          <SelectLabel inputProps={{ className:'form-control select', name : 'district', required : true}} 
-          labelName="ตำบล" iconProps={{className : 'fa icon icon-home'}} options={optionDistrict}  />
+          <div className="form-group">
+            <label>ตำบล</label>}
+            <select className="form-control" name={`district`} onChange={onChangeDistrict}>
+              {
+                district ? Object.keys(district).map((dis,index)=>(
+                  <option value={dis} key={dis}>{dis}</option>
+                )) : ''
+              }
+            </select>
+          </div>
         </div>
         <div className="col-lg-3 col-12">
-          <SelectLabel inputProps={{ className:'form-control select', name : 'zipcode', required : true}} 
-          labelName="รหัสไปรษณีย์" iconProps={{className : 'fa icon icon-home'}} options={optionPost}  />
+          <div className="form-group">
+            <label>รหัสไปรษณีย์</label>}
+            <select className="form-control" name={`post`} onChange={onChangeDistrict}>
+              {
+                zipcode ? Object.keys(zipcode).map((zip)=>(
+                  <option value={zip} key={zip}>{zip}</option>
+                )) : ''
+              }
+            </select>
+          </div>
         </div>
       </div>
     </>
