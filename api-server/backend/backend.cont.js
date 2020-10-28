1ac021dbed5fd0f3ed66431cbba132fbc05ef415
coldzify1 <39047015+coldzify1@users.noreply.github.com>
2020-10-28 17:45:59 +0700

    update

--- api-server/backend/backend.cont.js
+++ api-server/backend/backend.cont.js
@@ -3,10 +3,10 @@ const {User} = require('../db')
 
 exports.countBackend = async(req,res,next)=>{
   try{
-      const [pending] = await Promise.all([
+      const [fit_pending] = await Promise.all([
           User.count({where : { approve_status : 0,user_type : 'partner'}})
       ])
-      res.json({pending})
+      res.json({fit_pending})
   }
   catch(err){
       next(err);
