f666950cc6b2fc1ee7b168c10fca37fd5e07c372
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-11-05 15:59:55 +0700

    update

--- components/backend/table/TableUser.js
+++ components/backend/table/TableUser.js
@@ -1,9 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link'
 import api from '../../../utils/api-admin'
+import DataTable from 'react-data-table-component'
+import {toDateISO} from '../../../utils/tools'
+import SubHeaderComponent from './SubHeaderComponent'
+
+const columns = [
+  {
+    name: 'Company Name',
+    selector: 'company_name_en',
+    sortable: true,
+    cell : row => (
+      <div className="d-flex align-items-center my-2">
+        <div>
+          <img src={row.image_logo ? row.image_logo : "/template/img/no-picture.png"} className="approve-logo-table" />
+        </div>
+        <div className="ml-3">
+          <p className="mb-0">{row.company_name_en}</p>
+          <p className="mb-0 font-14">{row.company_name_th}</p>
+        </div>
+      </div>
+    )
+  },
+  {
+    name: 'License NO.',
+    selector: 'license_no',
+    sortable: true,
+    cell : row => (
+      <div>
+        <p className="mb-0">{row.license_no}</p>
+        <p className="mb-0 font-14">Expired Date : {row.license_expired_date ? toDateISO(row.license_expired_date) : null}</p>
+      </div>
+    )
+  },
+  {
+    name: 'Phone',
+    selector: 'company_phone',
+    sortable: true,
+  },
+  {
+    name: 'Email',
+    selector: 'company_email',
+    sortable: true,
+  },
+  {
+    name: 'Name',
+    sortable: true,
+    cell : row => <div>{row.firstname} {row.lastname}</div>
+  },
+  {
+    name: '',
+    sortable: true,
+    cell : row => (
+      <div>
+        <ul className="buttons manage">
+          <li>
+            <Link href="/backend/users/manage/[id]" as={`/backend/users/manage/${row.id}`}>
+              <a className="a-manage warning"><i className="fa fa-fw fa-cog"></i> <span>Manage</span></a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    )
+  },
+];
 
 const TableUser = (props) => {
   const [users, setUsers] = useState(false);
+  const [filterText, setFilterText] = useState('');
+  const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
 
   const fecthUsers = () => {
     api.getUsers({user_type : 'partner', approve_status : 1})
@@ -21,47 +86,24 @@ const TableUser = (props) => {
     fecthUsers();
   },[]);
 
+  const filteredItems = users ? users.rows.filter(item => item.company_name_en && item.company_name_en.toLowerCase().includes(filterText.toLowerCase())) : [];
+
   return (
     <>
-      <div className="table-responsive">
-        <table className="table table-bordered" width="100%" cellSpacing="0">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Full name</th>
-              <th>Status</th>
-              <th>Created date</th>
-              <th className="text-center">Manage</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              users ? users.rows.map((val, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{val.firstname} {val.lastname}</td>
-                  <td>
-                    {val.approve_status == 0 && <span className="a-manage warning">Pending</span>}
-                    {val.approve_status == 1 && <span className="a-manage success">Approve</span>}
-                    {val.approve_status == 2 && <span className="a-manage danger">Problem</span>}
-                  </td>
-                  <td>{new Date(val.createdAt).toLocaleString() }</td>
-                  <td className="text-center">
-                    <ul className="buttons manage">
-                      <li>
-                        <Link href="/backend/users/manage/[id]" as={`/backend/users/manage/${val.id}`}>
-                          <a className="a-manage warning"><i className="fa fa-fw fa-cog"></i> <span>Manage</span></a>
-                        </Link>
-                      </li>
-                    </ul>
-                  </td>
-                </tr>
-              )) : <tr><td colSpan="100%">Data Not found!!!</td></tr>
-            }
-            
-          </tbody>
-        </table>
-      </div>
+      <DataTable
+        columns={columns}
+        data={filteredItems}
+        pagination
+        paginationResetDefaultPage={resetPaginationToggle}
+        noHeader
+        subHeader
+        subHeaderComponent={
+          <SubHeaderComponent 
+            setResetPaginationToggle={setResetPaginationToggle} resetPaginationToggle={resetPaginationToggle} 
+            setFilterText={setFilterText} filterText={filterText}
+          />
+        }
+      />
     </>
   )
 }
--- pages/backend/index.js
+++ pages/backend/index.js
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Layout from '../../components/backend/layout/Layout';
-import TableTest from '../../components/backend/table/TableTest';
 
 const Index = ({ t }) => {
 
-  useEffect(() => {
-    $('#dataTable').DataTable();
-    console.log('sss')
-  },[])
- 
   return (
     <>
       <Layout title="Dashboard" page_name="Dashboard">
-        <TableTest />
       </Layout>
     </>
   )
