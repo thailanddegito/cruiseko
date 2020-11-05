4828106276c24365301002b9591d5e94169e1a49
coldzify1 <39047015+coldzify1@users.noreply.github.com>
2020-11-05 19:38:35 +0700

    update

--- api-server/backend/company_type/company_type.model.js
+++ api-server/backend/company_type/company_type.model.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, type) => {
-  var CompanyType  = sequelize.define('products', {
+  var CompanyType  = sequelize.define('company_type', {
     id : {
       type: type.INTEGER,
       primaryKey: true,
