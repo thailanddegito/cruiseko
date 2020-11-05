6ddcff19b6a62755c2f3ac1e6699fcaf3209f98d
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-11-05 16:09:11 +0700

    update

--- components/backend/table/TableUser.js
+++ components/backend/table/TableUser.js
@@ -1,69 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link'
 import api from '../../../utils/api-admin'
 import DataTable from 'react-data-table-component'
-import {toDateISO} from '../../../utils/tools'
 import SubHeaderComponent from './SubHeaderComponent'
-
-const columns = [
-  {
-    name: 'Company Name',
-    selector: 'company_name_en',
-    sortable: true,
-    cell : row => (
-      <div className="d-flex align-items-center my-2">
-        <div>
-          <img src={row.image_logo ? row.image_logo : "/template/img/no-picture.png"} className="approve-logo-table" />
-        </div>
-        <div className="ml-3">
-          <p className="mb-0 font-weight-bold">{row.company_name_en}</p>
-          <p className="mb-0 font-14">{row.company_name_th}</p>
-        </div>
-      </div>
-    )
-  },
-  {
-    name: 'License NO.',
-    selector: 'license_no',
-    sortable: true,
-    cell : row => (
-      <div>
-        <p className="mb-0 font-weight-bold">{row.license_no}</p>
-        <p className="mb-0 font-14">Expired Date : {row.license_expired_date ? toDateISO(row.license_expired_date) : null}</p>
-      </div>
-    )
-  },
-  {
-    name: 'Phone',
-    selector: 'company_phone',
-    sortable: true,
-  },
-  {
-    name: 'Email',
-    selector: 'company_email',
-    sortable: true,
-  },
-  {
-    name: 'Name',
-    sortable: true,
-    cell : row => <div>{row.firstname} {row.lastname}</div>
-  },
-  {
-    name: '',
-    sortable: true,
-    cell : row => (
-      <div>
-        <ul className="buttons manage">
-          <li>
-            <Link href="/backend/users/manage/[id]" as={`/backend/users/manage/${row.id}`}>
-              <a className="a-manage warning"><i className="fa fa-fw fa-cog"></i> <span>Manage</span></a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    )
-  },
-];
+import ColumnTable from '../user/ColumnTable'
 
 const TableUser = (props) => {
   const [users, setUsers] = useState(false);
@@ -91,7 +30,7 @@ const TableUser = (props) => {
   return (
     <>
       <DataTable
-        columns={columns}
+        columns={ColumnTable}
         data={filteredItems}
         pagination
         paginationResetDefaultPage={resetPaginationToggle}
