7a0ddc0f87dd8a2d6572f37bdb90e7ad7cc3dc7d
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-11-05 16:49:03 +0700

    update

--- components/backend/layout/Layout.js
+++ components/backend/layout/Layout.js
@@ -20,6 +20,9 @@ const Layout = (props) => {
         <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800" rel="stylesheet" />
 
         <link href="/backend_css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+
         <link href="/backend_css/css/admin.css" rel="stylesheet" />
         <link href="/backend_css/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
        
--- pages/backend/index.js
+++ pages/backend/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Layout from '../../components/backend/layout/Layout';
+import Slick from '../../components/backend/user/SlickTest'
 
 const Index = ({ t }) => {
 
   return (
     <>
       <Layout title="Dashboard" page_name="Dashboard">
+        <Slick />
       </Layout>
     </>
   )
