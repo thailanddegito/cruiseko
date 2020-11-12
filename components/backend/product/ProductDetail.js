2f4c8933939b9bd044675133baa6b17185f604f8
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-11-12 15:38:40 +0700

    update

--- components/backend/product/ProductDetail.js
+++ components/backend/product/ProductDetail.js
@@ -6,6 +6,10 @@ import SelectLabel from '../../widget/SelectLabel';
 import Dropzone from '../../widget/Dropzone'
 import api from '../../../utils/api-admin'
 import DivLoad from '../../../components/widget/DivLoad';
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated';
+
+const animatedComponents = makeAnimated();
 
 const Loading = <div className="position-relative"><DivLoad loading={true} /></div>;
 const Editor = dynamic(() => import('../../../components/widget/Editor'),{ ssr: false, loading: () => Loading })
@@ -29,6 +33,11 @@ const ProductDetail = (props) => {
   useEffect(() => {
     fecthProductCate();
   },[]);
+
+  var options = [{ value: '1', label: 'สินค้า CU'}, { value: '2', label: 'E-book'}, { value: '4', label: 'Course Online'}];
+
+  
+
 
   return (
     <>
@@ -66,6 +75,20 @@ const ProductDetail = (props) => {
             name : 'remark', required : true
           }} 
           labelName="Remark" iconProps={{className : 'fa icon icon-email'}}  />
+        </div>
+        <div className="col-lg-4 col-12">
+          <div className="form-group select2">
+            <label className="">Boat</label>
+            <Select
+              closeMenuOnSelect={false}
+              components={animatedComponents}
+              isMulti={false}
+              placeholder="-- Please Select Boat --"
+              name={"boat"}
+              options={options}
+              // onChange={(e) => handleChange(e)}
+            /> 
+          </div>
         </div>
       </div>
     </>
