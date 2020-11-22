f467c6e86325e67efdd3b2fbda1a600488df9eaf
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-11-22 20:57:03 +0700

    Update EventShow.js

--- components/backend/package/EventShow.js
+++ components/backend/package/EventShow.js
@@ -6,9 +6,14 @@ const EventShow = memo((props) => {
 
   return (
     <>
-      <div className="row">
-        <div className="col-12">
-          show
+      <div className="row div-table">
+        <div className="col-4">Title</div>
+        <div className="col-4">Time Title</div>
+        <div className="col-4">
+          <div className="text-right">
+            <button className="a-manage warning"><i className="fa fa-fw fa-pencil"></i> <span>See more</span></button>
+            <button className="a-manage danger"><i className="fa fa-fw fa-trash"></i> <span>Delete</span></button>            
+          </div>
         </div>
       </div>
     </>
