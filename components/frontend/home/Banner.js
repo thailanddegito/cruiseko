7f009ded5bd5fe0df1885c554d419b6580d79741
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-11-26 21:38:20 +0700

    update

---
+++ components/frontend/home/Banner.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Banner = (props) => {
+  const {data} = props;
+
+  return (
+    data ? (
+      <>
+        <section class="hero_single version_2">
+          <div class="wrapper">
+            <div class="container">
+              <h3>Book unique experiences</h3>
+              <p>Expolore top rated tours, hotels and restaurants around the world</p>
+              <form>
+                <div class="row no-gutters custom-search-input-2">
+                  <div class="col-lg-4">
+                    <div class="form-group">
+                      <input class="form-control" type="text" placeholder="Hotel, City..." />
+                      <i class="icon_pin_alt"></i>
+                    </div>
+                  </div>
+                  <div class="col-lg-3">
+                    <div class="form-group">
+                      <input class="form-control" type="text" name="dates" placeholder="When.." />
+                      <i class="icon_calendar"></i>
+                    </div>
+                  </div>
+                  <div class="col-lg-3">
+                    <div class="panel-dropdown">
+                      <a href="#">Guests <span class="qtyTotal">1</span></a>
+                      <div class="panel-dropdown-content">
+                        <div class="qtyButtons">
+                          <label>Adults</label>
+                          <input type="text" name="qtyInput" value="1" />
+                        </div>
+                        <div class="qtyButtons">
+                          <label>Childrens</label>
+                          <input type="text" name="qtyInput" value="0" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-2">
+                    <input type="submit" class="btn_search" value="Search" />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+      </>
+    ) : null
+  )
+}
+export default Banner
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/frontend/layout/Layout';
+import Banner from '../components/frontend/home/Banner'
+import ProductCard from '../components/frontend/product/ProductCard'
+import api from '../utils/api'
 
 const Home = (props) => {
   const [loading, setLodding] = useState(false);
+  const [packages, setPackage] = useState();
+
+  const fecthPackage = () => {
+    setLodding(true);
+    api.getPackage({is_draft : 0 , publish_status : 1})
+    .then(res=>{
+      const data = res.data;
+      setPackage(data);
+      setLodding(false);
+    })
+    .catch(err => {
+      setLodding(false);
+      console.log(err.response);
+    })
+  }
+  
+  useEffect(() => {
+    fecthPackage();
+  }, [])
 
   return (
     <Layout loading={loading} title="Home">
+      <aside className="main-content">
+				<main>
+					<div>
+            <Banner data={true} />
+					</div>
+					<div className="container">
+            <div className="wrapper-grid">
+              <div className="row">
+                {
+                  (packages && packages.rows) ? packages.rows.map((val, index) => (
+                    <ProductCard key={val.id} packages={val} />
+                  )) : null
+                }
+              </div>
+            </div>
+          </div>
+				</main>
+			</aside>
     </Layout>
   )
 }
