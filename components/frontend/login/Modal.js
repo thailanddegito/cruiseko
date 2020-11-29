2b2e521b3300e3c959f5e717ad330408a0ebb90d
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-11-29 17:48:47 +0700

    update

--- components/frontend/login/Modal.js
+++ components/frontend/login/Modal.js
@@ -6,6 +6,7 @@ import { Modal } from 'react-bootstrap';
 import UserContext from '../../../contexts/UserContext';
 import api from '../../../utils/api';
 import AuthService from '../../../utils/AuthService';
+import FacebookLogin from 'react-facebook-login'
 
 const Dialog = (props) => {
   const {show, size, onHide} = props;
@@ -33,19 +34,32 @@ const Dialog = (props) => {
       setError(err.response.data.error)
     })
   }
+
+  const responseFacebook = (res) => {
+    console.log(res);
+  }
   
   return (
     <Modal className="modal-alert" id="sign-in-dialog" centered show={show} onHide={onHide} size={size}>
       <Modal.Body>
         <div className="row">
           <div className="col-12">
-            <div class="small-dialog-header">
+            <div className="small-dialog-header">
               <h3>Sign In</h3>
             </div>
             <form className="px-3" id="login-form-modal" onSubmit={handleLogin}>
-              <div class="sign-in-wrapper">
-                <a href="#0" class="social_bt facebook">Login with Facebook</a>
-                <div class="divider"><span>Or</span></div>
+              <div className="sign-in-wrapper">
+                <FacebookLogin
+                  appId="1088597931155576"
+                  callback={responseFacebook}
+                  cssClass="social_bt facebook"
+                  render={renderProps => (
+                    // <a href="#" className="social_bt facebook" onClick={renderProps.onClick}>Login with Facebook</a>
+                    <button className="social_bt facebook" onClick={renderProps.onClick}>Login with Facebook</button>
+                  )}
+                />
+                {/* <a href="#0" className="social_bt facebook">Login with Facebook</a> */}
+                <div className="divider"><span>Or</span></div>
                 <InputLabel inputProps={{ 
                   className:'form-control', type : 'email',name : 'username', id : "email",
                   pattern : "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$", title :"Invalid email", required : true
@@ -61,24 +75,24 @@ const Dialog = (props) => {
                     <div className="text-danger"> Email or password is incorrect </div>
                   )
                 }
-                <div class="clearfix add_bottom_15">
-                  <div class="checkboxes float-left">
-                    <label class="container_check">Remember me
+                <div className="clearfix add_bottom_15">
+                  <div className="checkboxes float-left">
+                    <label className="container_check">Remember me
                       <input type="checkbox" />
-                      <span class="checkmark"></span>
+                      <span className="checkmark"></span>
                     </label>
                   </div>
                 </div>
-                <div class="text-center">
-                  <button type="submit" class="btn_1 full-width">Log In</button>
-                  {/* <input type="submit" value="Log In" class="btn_1 full-width" /> */}
+                <div className="text-center">
+                  <button type="submit" className="btn_1 full-width">Log In</button>
+                  {/* <input type="submit" value="Log In" className="btn_1 full-width" /> */}
                 </div>
-                <div class="text-center">
+                <div className="text-center">
                   Don’t have an account? <Link href="/register"><a>Sign up</a></Link>
                 </div>
               </div>
             </form>
-            <button title="Close (Esc)" type="button" class="mfp-close" onClick={onHide}></button>
+            <button title="Close (Esc)" type="button" className="mfp-close" onClick={onHide}></button>
           </div>
         </div>
       </Modal.Body>
