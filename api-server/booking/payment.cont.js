ef8cf6ddad99e8f46276081172e7fbaf992a18b9
coldzify1 <39047015+coldzify1@users.noreply.github.com>
2020-12-05 22:13:29 +0700

    update

--- api-server/booking/payment.cont.js
+++ api-server/booking/payment.cont.js
@@ -1,7 +1,8 @@
 const { Product,sequelize,PriceDate,
   CompanyType,ProductBoat,Booking,BookingDetail,PaypalHist,
   BookingBoat} = require('../db')
-
+const errors = require('../errors')
+const {DefaultError} = errors
 var paypal = require('paypal-rest-sdk')
 const config = {
   mode: process.env.NODE_ENV === 'dev' ? 'sandbox' : 'sandbox', //sandbox or live
@@ -17,8 +18,36 @@ exports.paypalApprove = async(req,res,next)=>{
   console.log('data',data)
   try{
 
-    const result = await verifyEvent(JSON.stringify(data) )
-    console.log(result)
+    const {event_type,resource} = data;
+
+    if(!resource){
+      throw new DefaultError(errors.FILEDS_INCOMPLETE);
+    }
+
+    if(event_type !== 'CHECKOUT.ORDER.APPROVED'){
+      return res.json({success:true})
+    }
+
+    const {purchase_units} = resource
+    const booking_id = purchase_units[0].invoice_id;
+
+
+    const [result,booking] = await Promise.all([
+      verifyEvent(JSON.stringify(data) ),
+      Booking.findAll({where : {id : booking_id}})
+    ])
+
+    if(!result || !booking){
+      throw new DefaultError(errors.INVALID_INPUT);
+    }
+
+    const {payment_status} = booking;
+    if(payment_status == 2){
+      return res.json({success:true})
+    }
+
+    await Booking.update({ payment_status : 2,payment_date : new Date(),payment_type : 'PAYPAL' },{id : booking_id})
+
     await PaypalHist.create({text : JSON.stringify({...data, verify_result : result })})
     res.json({success:true})
   }
