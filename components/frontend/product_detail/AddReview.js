9c06cc64239d6a088bdebb2bcc0d285d18535305
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2021-01-15 21:38:31 +0700

    update

--- components/frontend/product_detail/AddReview.js
+++ components/frontend/product_detail/AddReview.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import SelectLabel from '../../widget/SelectLabel';
 
 const AddReview = (props) => {
-  const {data} = props;
+  const {data, handleSave} = props;
+
+  const options = [{val : '1', name : '1 (lowest)'}, {val : '2', name : '2'}, {val : '3', name : '3'}, {val : '4', name : '4'}, {val : '5', name : '5 (medium)'}
+  , {val : '6', name : '6'}, {val : '7', name : '7'}, {val : '8', name : '8'}, {val : '9', name : '9'}, {val : '10', name : '10 (highest)'}];
+
 
   return (
     <>
       <div className="add-review">
         <h5>Leave a Review</h5>
-        <form>
+        <form onSubmit={handleSave}>
+          <input type="hidden" name="booking_id" defaultValue={data.booking_details[0]?.booking_id} />
           <div className="row">
-            <div className="form-group col-md-6">
-              <label>Name and Lastname *</label>
-              <input type="text" name="name_review" id="name_review" placeholder="" defaultValue={`${data.user_firstname} ${data.user_lastname}`} className="form-control" />
-            </div>
-            <div className="form-group col-md-6">
-              <label>Email *</label>
-              <input type="email" name="email_review" id="email_review" defaultValue={data.user_email} className="form-control" />
-            </div>
-            {/* <div className="form-group col-md-6">
-              <label>Rating </label>
+            <div className="col-md-6">
+              <SelectLabel 
+                inputProps={{ 
+                  className:'form-control', 
+                  name : 'rating', required : true,
+                }} 
+                labelName="Rating" icon={false} options={options} />
+              {/* <label>Rating </label>
               <div className="custom-select-form">
               <select name="rating_review" id="rating_review" defaultValue="1" className="wide">
                 <option value="1">1 (lowest)</option>
@@ -32,11 +36,11 @@ const AddReview = (props) => {
                 <option value="9">9</option>
                 <option value="10">10 (highest)</option>
               </select>
-              </div>
-            </div> */}
+              </div> */}
+            </div>
             <div className="form-group col-md-12">
               <label>Your Review</label>
-              <textarea name="review_text" id="review_text" className="form-control" style={{height : '130px'}}></textarea>
+              <textarea name="description" id="description" className="form-control" style={{height : '130px'}}></textarea>
             </div>
             <div className="form-group col-md-12 add_top_20">
               <input type="submit" value="Submit" className="btn_1" id="submit-review" />
